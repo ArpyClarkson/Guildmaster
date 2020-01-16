2806b195359966682469c7dfd0eba924f679622a
dan-wade <danwadeski@gmail.com>
2020-01-16 17:58:00 -0600

    ya yeet

--- Assets/Scripts/JSONDeserialize.cs
+++ Assets/Scripts/JSONDeserialize.cs
@@ -2,16 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using SimpleJson;
 
+public struct Point {
+	public float x, y;
+}
+
+public class Building {
+	public List<Point> points = new List<Point>();
+}
 
 public static class JSONDeserialize {
     static string jsonFile = "ARPICO.JSON";
 
 	[UnityEditor.MenuItem("Tools/Deserialize")]
-	public static void DoStuff() {
+	public static List<Building> DoStuff() {
 		string json = File.ReadAllText( jsonFile );
-		
 
+		dynamic root = SimpleJson.SimpleJson.DeserializeObject(json);
+		var buildings = new List<Building>();
 
+		foreach(var buildingData in root["features"][4]["geometry"]["coordinates"]) {
+			var building = new Building();
+			buildings.Add(building);
+
+			foreach(var pointData in buildingData[0]) {
+				building.points.Add(new Point{ x = pointData[0], y = pointData[1] });
+			}
+		}
+
+		return buildings;
     }
 }
