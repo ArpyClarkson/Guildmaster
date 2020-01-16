47f03d05d8dd2671fc05d819defdf638cff61e7f
dan-wade <danwadeski@gmail.com>
2020-01-16 16:55:57 -0600

    cleanup

--- Assets/Scripts/JSONDeserialize.cs
+++ Assets/Scripts/JSONDeserialize.cs
@@ -1,64 +1,17 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 
-[System.Serializable]
-public class Point {
-    public List<object> xy;
-}
 
-[System.Serializable]
-public class Coordinate {
-    public List<Point> points;
-}
+public static class JSONDeserialize {
+    static string jsonFile = "ARPICO.JSON";
 
-[System.Serializable]
-public class Geometry {
-    public string type;
-    public List<Coordinate> coordinates;
-}
+	[UnityEditor.MenuItem("Tools/Deserialize")]
+	public static void DoStuff() {
+		string json = File.ReadAllText( jsonFile );
+		
 
-[System.Serializable]
-public class Properties {
-    public string id;
-}
 
-[System.Serializable]
-public class Feature {
-    public string type;
-    public Geometry geometry;
-    public Properties properties;
-}
-
-[System.Serializable]
-public class RootObject {
-    public string type;
-    public List<Feature> features;
-}
-
-public class JSONDeserialize : MonoBehaviour {
-    string jsonfile = "ARPICO.JSON";
-    RootObject obj = new RootObject();
-
-    // Start is called before the first frame update
-    void Start() {
-        obj = Deserialize( jsonfile );
-
-        Debug.LogFormat( "JSON Deserialized" );
-        foreach (Feature f in obj.features) {
-            Debug.LogFormat( "Feature '{0}' loaded with {1} objects", f.properties.id, f.geometry.coordinates.Count );
-        }
-    }
-
-    // Update is called once per frame
-    void Update() {
-
-    }
-
-    public static RootObject Deserialize( string file ) {
-        string json = File.ReadAllText( file );
-
-        return JsonUtility.FromJson<RootObject>( json );
     }
 }
